b299e13e5ed43d6cdf0504f98489ff62a2085dd8
rodion-radov <rradov@alliedtesting.com>
2021-07-30 12:46:48 +0300

    Queue implementation

---
+++ src/data_structure/arrays/queue/Main.java
@@ -0,0 +1,22 @@
+package data_structure.arrays.queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        QueueImpl queue = new QueueImpl();
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+
+        queue.dequeue();
+
+
+        System.out.println(queue);
+
+    }
+
+}
---
+++ src/data_structure/arrays/queue/QueueImpl.java
@@ -0,0 +1,47 @@
+package data_structure.arrays.queue;
+
+import java.util.Queue;
+import java.util.Stack;
+
+public class QueueImpl {
+
+    //use 2 stacks
+
+    Stack<Integer> enqueue = new Stack<>();
+    Stack<Integer> dequeue = new Stack<>();
+
+    public void enqueue(int item){
+        enqueue.push(item);
+    }
+
+    public int dequeue(){
+        if(dequeue.empty() && enqueue.empty()) throw new IllegalStateException();
+
+            while(!enqueue.empty()){
+                dequeue.push(enqueue.pop());
+            }
+
+        return dequeue.pop();
+    }
+
+    public static void reverse(Queue<Integer> queue){
+      // q [10 ,20 ,30 ]
+      // s [30, 20, 10]
+        Stack<Integer> stack = new Stack<>();
+        while (!queue.isEmpty()){
+            stack.push(queue.remove());
+        }
+
+        while(!stack.empty()){
+            queue.add(stack.pop());
+        }
+
+    }
+
+
+
+    @Override
+    public String toString() {
+        return String.valueOf(dequeue);
+    }
+}
