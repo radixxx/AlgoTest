71735c55b25258b327897a6612ad808222b43426
rodion-radov <rradov@alliedtesting.com>
2021-08-02 14:24:02 +0300

    add method remove

--- src/data_structure/arrays/map/HashTableImpl.java
+++ src/data_structure/arrays/map/HashTableImpl.java
@@ -1,5 +1,6 @@
 package data_structure.arrays.map;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class HashTableImpl {
@@ -12,45 +13,59 @@ public class HashTableImpl {
             this.key = key;
             this.value = value;
         }
-
-
-        private LinkedList<Entry>[] entries = new LinkedList[100];
-
-
-        public void put(int key, String value) {
-            var index = hash(key);
-            if (entries[index] == null)
-                entries[index] = new LinkedList<>();
-
-            var bucket = entries[index];
-            for (var entry : bucket) {
-                if (entry.key == key)
-                    entry.value = value;
-                return;
-            }
-
-            entries[index].addLast(new Entry(key, value));
-        }
-
-
-        public String get(int key) {
-            var index = hash(key);
-            var bucket = entries[index];
-
-            if (bucket != null) {
-                for (var entry : bucket) {
-                    if (entry.key == key)
-                        return entry.value;
-                }
-            }
-
-            return null;
-
-        }
-
-        private int hash(int key) {
-            return key % entries.length;
-        }
     }
 
+    private LinkedList<Entry>[] entries = new LinkedList[10];
+
+    public void put(int key, String value) {
+        var index = hash(key);
+        if (entries[index] == null)
+            entries[index] = new LinkedList<>();
+
+        var bucket = entries[index];
+        for (var entry : bucket) {
+            if (entry.key == key)
+                entry.value = value;
+            return;
+        }
+
+        entries[index].addLast(new Entry(key, value));
+    }
+
+
+    public String get(int key) {
+        var index = hash(key);
+        var bucket = entries[index];
+
+        if (bucket != null) {
+            for (var entry : bucket) {
+                if (entry.key == key)
+                    return entry.value;
+            }
+        }
+
+        return null;
+    }
+
+    public void remove(int key){
+        var index = hash(key);
+        var bucket = entries[index];
+
+        if(bucket == null) throw new IllegalStateException();
+
+        for (var entry : bucket) {
+            if(entry.key == key){
+                bucket.remove(entry);
+                return;
+            }
+        }
+
+        throw new IllegalStateException();
+    }
+
+    private int hash(int key) {
+        return key % entries.length;
+    }
 }
+
+
