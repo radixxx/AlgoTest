9e9035a5f0d33a99eba52e5ffc264e763ce51990
rodion-radov <rradov@alliedtesting.com>
2021-08-05 13:34:15 +0300

    reformat

--- src/data_structure/arrays/queue/QueueImpl.java
+++ src/data_structure/arrays/queue/QueueImpl.java
@@ -10,35 +10,33 @@ public class QueueImpl {
     Stack<Integer> enqueue = new Stack<>();
     Stack<Integer> dequeue = new Stack<>();
 
-    public void enqueue(int item){
+    public void enqueue(int item) {
         enqueue.push(item);
     }
 
-    public int dequeue(){
-        if(dequeue.empty() && enqueue.empty()) throw new IllegalStateException();
+    public int dequeue() {
+        if (dequeue.empty() && enqueue.empty()) throw new IllegalStateException();
 
-            while(!enqueue.empty()){
-                dequeue.push(enqueue.pop());
-            }
+        while (!enqueue.empty()) {
+            dequeue.push(enqueue.pop());
+        }
 
         return dequeue.pop();
     }
 
-    public static void reverse(Queue<Integer> queue){
-      // q [10 ,20 ,30 ]
-      // s [30, 20, 10]
+    public static void reverse(Queue<Integer> queue) {
+        // q [10 ,20 ,30 ]
+        // s [30, 20, 10]
         Stack<Integer> stack = new Stack<>();
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             stack.push(queue.remove());
         }
 
-        while(!stack.empty()){
+        while (!stack.empty()) {
             queue.add(stack.pop());
         }
 
     }
-
-
 
     @Override
     public String toString() {
