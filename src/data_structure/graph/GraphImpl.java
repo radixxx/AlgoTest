adbb2ac916eb97d5d3b85235a3947491413e2a2b
rodion-radov <rradov@alliedtesting.com>
2021-09-06 12:33:11 +0300

    add "remove" + "removeEdge"

--- src/data_structure/graph/GraphImpl.java
+++ src/data_structure/graph/GraphImpl.java
@@ -31,23 +31,43 @@ public class GraphImpl {
     }
 
 
-    public void addEdge(String from, String to){
+    public void addEdge(String from, String to) {
         var fromNode = nodes.get(from);
-        if(fromNode == null) throw new IllegalArgumentException();
+        if (fromNode == null) throw new IllegalArgumentException();
 
         var toNode = nodes.get(to);
-        if(toNode == null) throw new IllegalStateException();
+        if (toNode == null) throw new IllegalStateException();
 
         adjacencyList.get(fromNode).add(toNode);
     }
 
 
-    public void print(){
-        for (var source: adjacencyList.keySet()) {
+    public void print() {
+        for (var source : adjacencyList.keySet()) {
             var targets = adjacencyList.get(source);
-            if(!targets.isEmpty()) System.out.println(source + "is connected to" + targets);
+            if (!targets.isEmpty()) System.out.println(source + " is connected to " + targets);
         }
     }
 
+    public void remove(String label) {
+        var node = nodes.get(label);
+        if (node == null) return;
+
+        for (var n : adjacencyList.keySet())
+            adjacencyList.get(n).remove(node);
+
+        adjacencyList.remove(node);
+        nodes.remove(node);
+    }
+
+
+    public void removeEdge(String from, String to) {
+        var fromNode = nodes.get(from);
+        var toNode = nodes.get(to);
+
+        if (from == null || to == null) return;
+
+        adjacencyList.get(fromNode).remove(toNode);
+    }
 
 }
--- src/data_structure/graph/Main.java
+++ src/data_structure/graph/Main.java
@@ -2,5 +2,19 @@ package data_structure.graph;
 
 public class Main {
 
+    public static void main(String[] args) {
+
+        var graph = new GraphImpl();
+
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+
+        graph.print();
+
+    }
 
 }
