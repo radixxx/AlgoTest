dca7598e0d7468f717ec9d5358e618c7715a5760
rodion-radov <rradov@alliedtesting.com>
2021-09-12 09:58:45 +0300

    refactoring 'getMinimumSpanningTree'

--- src/data_structure/graphUndirected/Main.java
+++ src/data_structure/graphUndirected/Main.java
@@ -5,17 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
         var graph = new WeightedGraph();
-
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
         graph.addEdge("A", "B", 3);
         graph.addEdge("B", "C", 5);
         graph.addEdge("A", "C", 13);
+        var ress = graph.getMinimumSpanningTree();
+        ress.print();
 
-
-       var result = graph.getShortestDistance("C", "B");
-
+        //var result = graph.getShortestDistance("C", "B");
         graph.print();
 
     }
