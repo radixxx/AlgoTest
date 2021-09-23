0b0935e9f6519b495da34dcec9d22dd2bc0f2471
rodion-radov <rradov@alliedtesting.com>
2021-09-23 12:32:14 +0300

    quick

--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -1,10 +1,6 @@
 package algorithms;
 
-import algorithms.sortings.Bubble;
-import algorithms.sortings.Insertion;
-import algorithms.sortings.Merge;
-import algorithms.sortings.Selection;
-import data_structure.arrays.Array;
+import algorithms.sortings.*;
 
 import java.util.Arrays;
 
@@ -14,19 +10,23 @@ public class Main {
         int [] numbers = {9, 4, 12, 5, 9};
         var bubble = new Bubble();
         bubble.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+        System.out.println("Bubble: " + Arrays.toString(numbers));
 
         var selection = new Selection();
         selection.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+        System.out.println("Selection: " + Arrays.toString(numbers));
 
         var insertion = new Insertion();
         insertion.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+        System.out.println("Insertion: " + Arrays.toString(numbers));
 
         var merge = new Merge();
         merge.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+        System.out.println("Merge: " + Arrays.toString(numbers));
+
+        var quick = new Quick();
+        quick.sort(numbers);
+        System.out.println("Quick: " + Arrays.toString(numbers));
 
     }
 
