fda32504ff2f6efc012af81beac2549f098375f6
rodion-radov <rradov@alliedtesting.com>
2021-10-03 18:42:22 +0300

    string utils

---
+++ src/algorithms/string_manipulation/Main.java
@@ -0,0 +1,11 @@
+package algorithms.string_manipulation;
+
+public class Main {
+    public static void main(String[] args) {
+
+       int count = StringUtils.countVowels("Hill");
+        System.out.println(count);
+
+    }
+
+}
---
+++ src/algorithms/string_manipulation/StringUtils.java
@@ -0,0 +1,17 @@
+package algorithms.string_manipulation;
+
+public class StringUtils {
+    public static int countVowels(String str){
+        if(str == null) return 0;
+
+        int count = 0;
+          String vowels = "aeiou";
+
+        for(var ch : str.toCharArray()){
+            if(vowels.indexOf(ch) != -1) count++;
+        }
+
+        return count;
+    }
+
+}
